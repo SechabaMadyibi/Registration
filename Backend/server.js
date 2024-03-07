ea794842e0419ab23c90ba8d73677877e0065f3f
SechabaMadyibi <sechabamadyibi1@gmail.com>
2024-03-07 22:59:26 +0200

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv").config();
 const cors = require("cors");
-const path = require('path')
 const routes = require("./Routes/routes.js")
 
 //connect to database
